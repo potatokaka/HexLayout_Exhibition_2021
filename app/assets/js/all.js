1d68f772b1ea41ec7cbc1b1c3a2ccd05baaa5225
potatokaka <potatokaka@gmail.com>
2021-08-17 20:42:36 +0800

    project submit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,8 +4,16 @@ $(function() {
 
 // Swiper
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
+
+  // 768px 以上
+  breakpoints: {
+    768:{
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  }
   
   /* 移除 pagination */
   // pagination: {
