f495731ee21c4e8b750e1d0f417fee78fffede08
potatokaka <potatokaka@gmail.com>
2021-08-17 20:44:31 +0800

    Update 2021-08-17T12:44:29.833Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,8 +5,15 @@ $(function () {
 }); // Swiper
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // 768px 以上
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
   /* 移除 pagination */
   // pagination: {
   //   el: ".swiper-pagination",
