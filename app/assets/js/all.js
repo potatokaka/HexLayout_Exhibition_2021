0544dcb94aaeaa6ccb86707163aab716404450db
potatokaka <potatokaka@gmail.com>
2021-08-17 15:02:26 +0800

    test commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,19 +2,14 @@ $(function() {
   console.log('Hello Bootstrap5');
 });
 
-// CK Editor
-ClassicEditor
-  .create( document.querySelector( '#editor' ) )
-  .then( editor => {
-  console.log( editor );
-} )
-.catch( error => {
-  console.error( error );
-} );
-
-// CK Editor 客製 Toggle
-$(document).ready(function(){
-  $('#btn-reply').click(function(){
-      $('.editorWrap').toggleClass('active');
-  });
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  
+  /* 移除 pagination */
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
 });
